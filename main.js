baad7d56e444e70d26d0bd03ed04ef0b2e9b27c4
Matheus Henrique Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2021-06-14 21:55:47 -0300

    adicionado a função editarTarefa

--- main.js
+++ main.js
@@ -1,56 +1,77 @@
 const elementoLista = document.querySelector('ul')
 const elementoImput = document.querySelector('input')
-const elementoBotao2 = document.getElementById('deletar tudo')
+const btnExcluirTudo = document.querySelector('#btnDeletarTudo')
+
+elementoImput.addEventListener('keypress', (e) => {
+    if (e.which == 13) {
+        adicionarTarefas()
+    }
+})
+
+btnExcluirTudo.setAttribute('onclick', 'deletarTudo()')
 
 const tarefas = []
 
 function mostrarTarefas() {
     elementoLista.innerHTML = null
     for (tarefa of tarefas) {
-        const elementoTarefa = document.createElement('li')
-        const textoTarefa = document.createTextNode(tarefa)
+        const lstTarefa = document.createElement('li')
+        const txtTarefa = document.createTextNode(tarefa)
+        const posTarefa = tarefas.indexOf(tarefa)
 
-        const elementoLink = document.createElement('a')
-        const pos = tarefas.indexOf(tarefa)
+        const btnEditar = document.createElement('button')
+        btnEditar.classList.add('btnEditar')
+        const icnEditar = document.createElement('i')
+        icnEditar.setAttribute('class', 'fa fa-pencil')
+        btnEditar.appendChild(icnEditar)
+        btnEditar.setAttribute('onclick', `editarTarefa(${posTarefa})`)
 
-        const linkText = document.createTextNode('x')
-        elementoLink.appendChild(linkText)
-        elementoLink.setAttribute('href', '#')
-        elementoLink.setAttribute('onclick', `removerTarefa(${pos})`)
+        const btnExcluir = document.createElement('button')
+        btnExcluir.classList.add('btnExcluir')
+        const icnExcluir = document.createElement('i')
+        icnExcluir.setAttribute('class', 'fa fa-trash')
+        btnExcluir.appendChild(icnExcluir)
+        btnExcluir.setAttribute('href', '#')
+        btnExcluir.setAttribute('onclick', `removerTarefa(${posTarefa})`)
 
 
-        elementoTarefa.appendChild(textoTarefa)
-        elementoLista.appendChild(elementoTarefa)
-        elementoTarefa.appendChild(elementoLink)
+        lstTarefa.appendChild(txtTarefa)
+        elementoLista.appendChild(lstTarefa)
+        lstTarefa.appendChild(btnEditar)
+        lstTarefa.appendChild(btnExcluir)
+
+
     }
 }
 
 function adicionarTarefas() {
-    const textoTarefa = elementoImput.value
+    const txtTarefa = elementoImput.value
 
-    if (textoTarefa != '') {
-        tarefas.push(textoTarefa)
+    if (txtTarefa != '') {
+        tarefas.push(txtTarefa)
+    } else {
+        alert("O texto não pode ser vazio")
     }
-    mostrarTarefas()
     elementoImput.value = null
+    mostrarTarefas()
+
 }
 
-function removerTarefa(pos) {
-    tarefas.splice(pos, 1)
+function removerTarefa(posTarefa) {
+    tarefas.splice(posTarefa, 1)
     mostrarTarefas()
+
+}
+
+function editarTarefa(posTarefa) {
+    alert(posTarefa)
+
 }
 
 function deletarTudo() {
     while (tarefas.length != 0) {
         tarefas.pop()
     }
+    elementoImput.value = null;
     mostrarTarefas()
 }
-
-elementoImput.onkeypress = function(e) {
-    if (e.keyCode == 13) {
-        adicionarTarefas()
-    }
-}
-
-elementoBotao2.setAttribute('onclick', 'deletarTudo()')
